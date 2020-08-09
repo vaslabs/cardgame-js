462dc832fb5f5145dfaa4fc38859b88d002383c9
Vasilis Nicolaou <vaslabsco@gmail.com>
2020-08-09 17:18:35 +0100

    persist vector clock locally

--- src/app/vector-clock.service.ts
+++ src/app/vector-clock.service.ts
@@ -11,6 +11,7 @@ export class VectorClockService {
 
 
   constructor(private cookieService: CookieService) {
+    this.recover()
   }
 
   tick(me: string): { [key:string]:number; } {
@@ -18,7 +19,7 @@ export class VectorClockService {
       this.vectorClock[me]++
     else
       this.vectorClock[me] = 0
-    
+    this.persist()
     return this.vectorClock
   }
 
@@ -33,6 +34,21 @@ export class VectorClockService {
     });
     this.serverClock = Math.max(this.serverClock, serverClock)
     this.tick(me)
+    this.persist()
     return this.vectorClock
+  }
+
+  persist() {
+    this.cookieService.set("vector-clock", JSON.stringify(this.vectorClock))
+    this.cookieService.set("server-clock", this.serverClock.toString())
+  }
+
+  recover() {
+    const vectorClock: string = this.cookieService.get("vector-clock")
+    const serverClock: string = this.cookieService.get("server-clock")
+    if (vectorClock)
+      this.vectorClock = JSON.parse(vectorClock)
+    if (serverClock)
+      this.serverClock = parseInt(serverClock)
   }
 }
